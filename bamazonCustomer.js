183fddd04fd559654a15fda42911614557316161
Karen Largent <karenlargent@Karens-MacBook-Pro.local>
2019-07-11 16:40:40 -0400

    product list is a global variable that populates from the DB

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,6 +1,8 @@
+// INCLUDING NPM PACKAGES 
 var mysql = require("mysql"); 
 var inquirer = require("inquirer"); 
 
+// DATABASE CONNECTION VAR AND FUNCTION 
 var connection = mysql.createConnection ({
     host: "localhost", 
     port: 3306, 
@@ -14,22 +16,34 @@ connection.connect(function(err) {
     console.log ("You're in Bamazon!"); 
 })
 
+// GLOBAL VARIABLES 
+var productList = []; 
+
 productDisplay(); 
 
+// RETRIEVE A LIST OF PRODUCTS AND SAVING THEIR NAMES TO AN EMPTY ARRAY 
 function productDisplay () { 
     var query = "SELECT * FROM products"; 
     connection.query (query, function (err, result) { 
         if (err) throw err; 
-        console.log(result); 
-    })
+        // console.log(result); 
+        
+        // var productList = []; 
+        for (var i = 0; i < result.length; i++)
+            // console.log(result[i].product_name); 
+            productList.push(result[i].item_id, result[i].product_name); 
+            console.log(productList); 
+    }) 
 }
 
 // function welcome () { 
 //     inquirer    
-//         .prompt ({
-//             name: "welcome", 
-//             type: "list", 
-//             message: "Welcome to Bamazon! Here are today's Thunderbolt Deals! What would you like to purchase?", 
-//             choices: 
-//         })
+//         .prompt ([
+//             {
+//                 name: "welcome", 
+//                 type: "list", 
+//                 message: "Welcome to Bamazon! Here are today's Thunderbolt Deals! What would you like to purchase?", 
+//                 choices: 
+//             }
+//         ])
 // }
