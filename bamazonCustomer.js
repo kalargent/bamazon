fcadd378fbc9ff5a6d173d9fe151bc0d61022694
Karen Largent <karenlargent@Karens-MacBook-Pro.local>
2019-07-11 16:21:34 -0400

    retrieving a list of items from the database

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -13,3 +13,23 @@ connection.connect(function(err) {
     if (err) throw err; 
     console.log ("You're in Bamazon!"); 
 })
+
+productDisplay(); 
+
+function productDisplay () { 
+    var query = "SELECT * FROM products"; 
+    connection.query (query, function (err, result) { 
+        if (err) throw err; 
+        console.log(result); 
+    })
+}
+
+// function welcome () { 
+//     inquirer    
+//         .prompt ({
+//             name: "welcome", 
+//             type: "list", 
+//             message: "Welcome to Bamazon! Here are today's Thunderbolt Deals! What would you like to purchase?", 
+//             choices: 
+//         })
+// }
