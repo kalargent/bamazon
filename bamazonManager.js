1c048735062e7c342ec74955f867034baadee4c6
Karen Largent <karenlargent@Karens-MacBook-Pro.local>
2019-07-15 12:55:18 -0400

    bamazon manager connects to the database

---
+++ bamazonManager.js
@@ -0,0 +1,18 @@
+// INCLUDING NPM PACKAGES
+var mysql = require("mysql");
+var inquirer = require("inquirer");
+// var Table = require("cli-table2");
+require("console.table"); 
+
+var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "",
+    database: "bamazon"
+  });
+  
+  connection.connect(function(err) {
+    if (err) throw err;
+    console.log ("You're a manager!");
+  });
